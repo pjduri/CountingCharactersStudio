1c3cad663199086336aeb45edcbd76816ee07e72
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-14 12:13:36 -0500

    first commit

---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+Dictionary<char, int> charCounts = [];
+
+foreach (char letter in loremIpsum.ToUpper().ToCharArray())
+{
+    if (!charCounts.ContainsKey(letter) && char.IsLetter(letter))
+    {
+        charCounts.Add(letter, 1);
+    }
+    else if (char.IsLetter(letter))
+    {
+        charCounts[letter]++;
+    }
+}
+
+foreach (KeyValuePair<char, int> entry in charCounts)
+{
+    Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
---
+++ SuperBonus/Program.cs
@@ -0,0 +1,37 @@
+﻿string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+string fileText = File.ReadAllText(@"./sampleText.txt");
+
+Console.WriteLine("Enter some text, or you can leave blank");
+Console.WriteLine("to tally up some song lyrics,");
+Console.WriteLine("or type 'L' to get the good ol' Lorem Ipsum");
+
+string? theText = Console.ReadLine() ?? "";
+if (theText == "") 
+{
+    theText = fileText;
+}
+
+if (theText.ToUpper().Equals("L")) 
+{
+    theText = loremIpsum;
+}
+
+Dictionary<char, int> charCounts = [];
+
+foreach (char letter in theText.ToUpper().ToCharArray())
+{
+    if (!charCounts.ContainsKey(letter) && char.IsLetter(letter))
+    {
+        charCounts.Add(letter, 1);
+    }
+    else if (char.IsLetter(letter))
+    {
+        charCounts[letter]++;
+    }
+}
+
+foreach (KeyValuePair<char, int> entry in charCounts)
+{
+    Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
